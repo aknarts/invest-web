e98f3f4d6e522af8c1d4e9a6ed330ccd0185e261
Milan Stastny <milan@stastnej.ch>
2022-09-04 23:36:11 +0200

    Trying to implement the router

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,99 @@
+use crate::pages::home::Home;
+use crate::pages::page_not_found::PageNotFound;
+use yew::html::Scope;
+use yew::html::*;
 use yew::prelude::*;
+use yew_router::prelude::*;
 
-#[function_component(App)]
-pub fn app() -> Html {
-    html! {
-        <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
-        </main>
+#[derive(Routable, PartialEq, Eq, Clone, Debug)]
+pub enum Route {
+    #[at("/")]
+    Home,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+pub enum Msg {
+    ToggleNavbar,
+}
+
+pub struct App {
+    navbar_active: bool,
+}
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            navbar_active: false,
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::ToggleNavbar => {
+                self.navbar_active = !self.navbar_active;
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <BrowserRouter>
+                { self.view_nav(ctx.link()) }
+
+                <main>
+                    <Switch<Route> render={switch} />
+                </main>
+                <footer class="footer">
+                    <div class="content has-text-centered">
+                        { "Powered by " }
+                        <a href="https://yew.rs">{ "Yew" }</a>
+                    </div>
+                </footer>
+            </BrowserRouter>
+        }
+    }
+}
+
+impl App {
+    fn view_nav(&self, link: &Scope<Self>) -> Html {
+        let Self { navbar_active, .. } = *self;
+
+        let active_class = if !navbar_active { "is-active" } else { "" };
+
+        html! {
+            <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <h1 class="navbar-item is-size-3">{ "Yew Blog" }</h1>
+
+                    <button class={classes!("navbar-burger", "burger", active_class)}
+                        aria-label="menu" aria-expanded="false"
+                        onclick={link.callback(|_| Msg::ToggleNavbar)}
+                    >
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </button>
+                </div>
+                <div class={classes!("navbar-menu", active_class)}>
+                    <div class="navbar-start">
+                        <Link<Route> classes={classes!("navbar-item")} to={Route::Home}>
+                            { "Home" }
+                        </Link<Route>>
+                    </div>
+                </div>
+            </nav>
+        }
+    }
+}
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => return html! { <Home /> },
+        Route::NotFound => return html! { <PageNotFound /> },
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod app;
-
+mod pages;
 use app::App;
 
 fn main() {
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
     yew::start_app::<App>();
 }
